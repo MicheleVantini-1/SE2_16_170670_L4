b806787337e52abc0dc28ed5f6bc376675bd1c6a
michele <michele.vantini-1@studenti.unitn.it>
2016-11-23 09:08:16 +0100

    serverConfig.js with server parameters added;

---
+++ serverConfig.js
@@ -0,0 +1,28 @@
+// MODULE where to put all the constants
+// of the application
+
+// port number used as default 
+var port = 8080;
+// address used as default
+var address = "127.0.0.1";
+
+// all the headers that will be set in the 
+// http response header 
+var headers = {};
+
+// with this we allow any origin to access the resource
+headers["Access-Control-Allow-Origin"] = "*"; 
+// allowed methods
+headers["Access-Control-Allow-Methods"] = "POST, GET"; 
+headers["Access-Control-Allow-Credentials"] = false;
+headers["Access-Control-Max-Age"] = '86400';
+//types of headers that are allowed
+headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"; 
+// the format of the response 
+headers["Content-Type"] = "text/html";
+
+// EXPORTS
+exports.port = port;
+exports.address = address;
+exports.headers = headers;
+
