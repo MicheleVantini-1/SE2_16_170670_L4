6f5ccaf4848268bf4633712dca7438a230bf1e63
michele <michele.vantini-1@studenti.unitn.it>
2016-11-23 14:57:46 +0100

    added client side control and placeholders for error messages

--- script/script.js
+++ script/script.js
@@ -6,26 +6,37 @@
 + @param searchBool true in case of search; false in case of delete
 */
 function search(searchBool) {
-    var form = document.createElement("form");
+    var emp_search = document.getElementById("employee_search").value;
+    // at first we have to check the form input
+    if (emp_search == "" || isNaN(parseInt(emp_search)))
+    {
+        // if the field is empty or is not a number we return an error
+        document.getElementById("searchNaN").innerHTML = "Id must be an integer number";
+        document.getElementById("insertModifyForm").style.display = "none";        
+    }
+    else
+    {
+        var form = document.createElement("form");
 
-    var search = document.createElement("input"); 
-    search.type = "text";
-    search.value = document.getElementById("employee_search").value;
-    search.name = "employee_search";
-    search.id = "employee_search";
-    
-    var isSearch = document.createElement("input");  
-    isSearch.type = "text";
-    isSearch.value = searchBool;
-    isSearch.name = "isSearch";
-    isSearch.id = "isSearch";
-    
-    form.appendChild(search);  
-    form.appendChild(isSearch);
+        var search = document.createElement("input"); 
+        search.type = "text";
+        search.value = document.getElementById("employee_search").value;
+        search.name = "employee_search";
+        search.id = "employee_search";
+        
+        var isSearch = document.createElement("input");  
+        isSearch.type = "text";
+        isSearch.value = searchBool;
+        isSearch.name = "isSearch";
+        isSearch.id = "isSearch";
+        
+        form.appendChild(search);  
+        form.appendChild(isSearch);
 
-    form.method = "POST";
-    form.action = "http://localhost:8080/search"; 
-    form.submit();
+        form.method = "POST";
+        form.action = "http://localhost:8080/search"; 
+        form.submit();
+    }
 }
 
 /**
@@ -36,9 +47,76 @@ function search(searchBool) {
 */
 function insertModifySubmit()
 {
-    // here we will put the different checks on the input fields
+    // we have to check each value of the input fields
 
-    document.getElementById("insertModifyForm").submit();
+    var id = document.getElementById("emp_id").value;
+    var name = document.getElementById("emp_name").value;
+    var surname = document.getElementById("emp_surname").value;
+    var level = document.getElementById("emp_lv").value;
+    var salary = document.getElementById("emp_salary").value;
+
+    var error = false;
+    
+    if (id == "" || isNaN(parseInt(id)))
+    {
+        // if the id field is empty or is not a number we return an error
+        document.getElementById("idNaN").innerHTML = "Id must be an integer number";
+        error = true;
+    }
+    else
+    {
+        document.getElementById("idNaN").innerHTML = "";
+    }
+    
+    if (name == "")
+    {
+        // if the name field is empty we return an error
+        document.getElementById("nameEmpty").innerHTML = "The name must be not empty";
+        error = true;
+    }
+    else
+    {
+        document.getElementById("nameEmpty").innerHTML = "";
+    }
+    
+    if (surname == "")
+    {
+        // if the surname field is empty we return an error
+        document.getElementById("surnameEmpty").innerHTML = "The surname must be not empty";
+        error = true;
+    }
+    else
+    {
+        document.getElementById("surnameEmpty").innerHTML = "";
+    }
+    
+    if (level == "" || isNaN(parseInt(level)))
+    {
+        // if the level field is empty or is not a number we return an error
+        document.getElementById("levelNaN").innerHTML = "Level must be an integer number";
+        error = true;
+    }
+    else
+    {
+        document.getElementById("levelNaN").innerHTML = "";
+    }
+    
+    if (salary == "" || isNaN(parseInt(salary)))
+    {
+        // if the salary field is empty or is not a number we return an error
+        document.getElementById("salaryNaN").innerHTML = "Salary must be an integer number";
+        error = true;
+    }
+    else
+    {
+        document.getElementById("salaryNaN").innerHTML = "";
+    }
+
+    // if everything is ok we submit the form
+    if(!error)
+    {
+        document.getElementById("insertModifyForm").submit();
+    }
 }
 
 /**
--- tpl/index.tpl
+++ tpl/index.tpl
@@ -6,7 +6,7 @@
 	</head>
 	<body>
 		<h2> Employees manager </h2>
-		<label>ID:</label><input type="numeric" name="employee_search" id="employee_search"/>
+		<label>ID:</label><input type="numeric" name="employee_search" id="employee_search"/> <b id="searchNaN"> (: searchNaN ~ :) </b>
 		<button onclick="search(true);">Search</button>
 		<button onclick="search(false);">Delete</button>
 		<br/><br/>
@@ -15,15 +15,16 @@
 		<br/>
 		<form action="http://localhost:8080/modify" method='POST' id="insertModifyForm" (: formVisibility ~ style="display:none;" :) >
 			<label>ID:</label>
-			<input type="numeric" name="emp_id" id="emp_id" (: id ~ :) />
+			<input type="numeric" name="emp_id" id="emp_id" (: id ~ :) /> <b id="idNaN"> (: idNaN ~ :) </b>
 			<br/>
-			<label>Name:</label><input type="text" name="emp_name" id="emp_name" (: name ~ :)/> 
-			<label>Surname:</label><input type="text" name="emp_surname" id="emp_surname" (: surname ~ :)/>
+			<label>Name:</label><input type="text" name="emp_name" id="emp_name" (: name ~ :)/> <b id="nameEmpty"> (: nameEmpty ~ :) </b>
+			<label>Surname:</label><input type="text" name="emp_surname" id="emp_surname" (: surname ~ :)/> <b id="surnameEmpty"> (: surnameEmpty ~ :) </b>
 			<br/>
-			<label>Level:</label><input type="numeric" name="emp_lv" id="emp_lv" (: level ~ :)/> 
+			<label>Level:</label><input type="numeric" name="emp_lv" id="emp_lv" (: level ~ :)/> <b id="levelNaN"> (: levelNaN ~ :) </b>
 			<br/>
-			<label>Salary:</label><input type="numeric" name="emp_salary" id="emp_salary" (: salary ~ :)/> 
+			<label>Salary:</label><input type="numeric" name="emp_salary" id="emp_salary" (: salary ~ :)/> <b id="salaryNaN"> (: salaryNaN ~ :) </b>
 			<br/><br/>
+			<b>  (: errorMsg ~ :) <b><br/>
 			<button type="button" onclick="insertModifySubmit();">Insert/Modify</button>
 		</form>
 
